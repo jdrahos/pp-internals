172b40f98132d7558bfe7dca00bb4bc0d8e6a996
Pavel Moukhataev <m_pashka@mail.ru>
2016-02-18 15:50:15 +0300

    -

--- data/src/sql/impala1.sql
+++ data/src/sql/impala1.sql
@@ -13,4 +13,4 @@ select servername,count(*) from event.logevent where publisherid=558360 and day=
 
 select deflevelid,deflevelreason,clientimpression,serverimpression,count(*) from event.logevent where publisherid=558360 and day='2016-02-05' and servername='LGA-RTS00' group by deflevelid,deflevelreason,clientimpression,serverimpression;
 
-select servername,deflevelid,deflevelreason,clientimpression,serverimpression,count(*) from event.logevent where publisherid=558360 and day='2016-02-05' group by servername,deflevelid,deflevelreason,clientimpression,serverimpression;
+select servername,serverchain,deflevelid,deflevelreason,clientimpression,serverimpression,count(*) from event.logevent where servername='LGA-RTB00' and publisherid=558360 and day='2016-02-05' group by servername,serverchain,deflevelid,deflevelreason,clientimpression,serverimpression;
